30c9c89eaf0224e4da2e424b695cabc8b18231f2
Bobby <b.novak23964@gmail.com>
2018-01-04 23:43:22 -0500

    Created GameMaster and AudioManager scripts

---
+++ Project/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gm;
+
+    private void Awake()
+    {
+        if (gm == null)
+            gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameMaster>();
+    }
+
+    public Transform playerPrefab;
+}
