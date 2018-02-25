9094af4457eb722faaec1a76df62f4670f1a731d
Elliot Pajer <elliotpajer@dhcp-212-205.cc.gettysburg.edu>
2018-02-24 22:52:50 -0500

    (2/24/18)

---
+++ Project/Assets/Scripts/fallDeath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// THIS SCRIPT DOESN'T WORK!!! :DDDDD
+
+public class fallDeath : MonoBehaviour
+{
+	// Your variables from GameMaster for the player
+	public GameObject player;
+	public GameObject playerPrefab;
+	public static GameObject spawnPoint;
+
+	// Detects "Idle," respawns
+	void OnCollisionEnter2D (Collision other)
+	{
+		if (other.gameObject.name == "Idle")
+		{
+			Respawn ();
+		}
+	}
+
+	// Respawn function
+	IEnumerator Respawn ()
+	{
+		yield return new WaitForSeconds(2f);
+		player = Instantiate(playerPrefab, spawnPoint.transform.position, Quaternion.identity);
+	}
+}
