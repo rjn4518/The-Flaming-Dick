a0713ad071885838af714ee6bcb673369c9dcd08
Elliot Pajer <elliotpajer@Elliots-Air.hitronhub.home>
2018-08-17 06:02:52 -0400

    Health Bar (8/17/18)

---
+++ Project/Assets/Scripts/BarScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarScript : MonoBehaviour
+{
+	[SerializeField]
+	private float fillAmount;
+
+	[SerializeField]
+	private Image Health;
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		HandleBar();
+	}
+
+	private void HandleBar()
+	{
+		Health.fillAmount = fillAmount;
+		//Health fill currently on a scale from 0-1, ex. half health = 0.5
+	}
+}
