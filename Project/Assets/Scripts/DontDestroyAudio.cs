1157f28453e7db7296f087b6edd06b63401ad7a0
Elliot Pajer <elliotpajer@Elliots-MBP.hitronhub.home>
2019-01-03 00:46:23 -0500

    (1/3/19)

--- Project/Assets/Scripts/DontDestroyAudio.cs
+++ Project/Assets/Scripts/DontDestroyAudio.cs
@@ -6,6 +6,10 @@ public class DontDestroyAudio : MonoBehaviour
 {
 	void Awake ()
 	{
-		DontDestroyOnLoad (transform.gameObject);
+		GameObject[] objs = GameObject.FindGameObjectsWithTag ("Music");
+		if (objs.Length > 1)
+			Destroy (this.gameObject);
+
+		DontDestroyOnLoad (this.gameObject);
 	}
 }
