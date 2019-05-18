9124e4c2490e86b2ec10c2a063fbd72049a72af3
Bobby <b.novak23964@gmail.com>
2019-05-18 00:20:39 -0400

    Made garden project

---
+++ Penguin/Time/Assets/Scripts/GatterAbleObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GatterAbleObject : MonoBehaviour
+{
+    public int id,u;
+    public bool cc;
+    private void Start()
+    {
+        GetComponent<SpriteRenderer>().sprite = GameController.GC.Items[id];
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (u == 0)
+            {
+                u ++;
+                GameController.GC.ep++;
+                GameController.GC.gao = this;
+            }
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if (u != 0)
+            {
+                u = 0;
+                GameController.GC.ep--;
+            }
+        }
+    }
+    public void Bye()
+    {
+        Destroy(gameObject);
+    }
+}
