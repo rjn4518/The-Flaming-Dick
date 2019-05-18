cc6336f9e14fc3df180354fa8919624e5c7fe198
Bobby <b.novak23964@gmail.com>
2019-05-18 19:57:55 -0400

    Started controller script

---
+++ Strange_ass_garden/Garden/Assets/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    Vector3 mousePos;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        MouseFollow();
+	}
+
+    void MouseFollow()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = mousePos.z - Camera.main.transform.position.z;
+
+        transform.position = Vector3.Lerp(transform.position, mousePos, 0.1f);
+
+        Debug.Log(Input.mousePosition);
+    }
+        
+}
