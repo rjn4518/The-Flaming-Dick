f672d62ce07d8d1ce553277a73591d88c66e5601
Bobby <b.novak23964@gmail.com>
2019-05-19 14:03:37 -0400

    Got player following mouse

--- Strange_ass_garden/Garden/Assets/Scripts/Controller.cs
+++ Strange_ass_garden/Garden/Assets/Scripts/Controller.cs
@@ -20,12 +20,12 @@ public class Controller : MonoBehaviour
 
     void MouseFollow()
     {
-        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousePos.z = mousePos.z - Camera.main.transform.position.z;
+        mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+       // mousePos.z = mousePos.z - Camera.main.transform.position.z;
 
-        transform.position = Vector3.Lerp(transform.position, mousePos, 0.1f);
+        Debug.Log(mousePos);
 
-        Debug.Log(Input.mousePosition);
+        transform.position = Vector3.Lerp(transform.position, mousePos, 0.05f);
     }
         
 }
